73b751a3dfcc961dd03df187f605695b1b83fc24
Thomas Howe <ghostofbasho@gmail.com>
2025-09-25 17:21:41 -0400

    Add generated files and targets after tag creation

--- .targets.mk
+++ .targets.mk
@@ -2,6 +2,15 @@
 # 
 versioned:
 	@mkdir -p $@
-.INTERMEDIATE: versioned/draft-howe-vcon-wtf-extension-00.xml
-versioned/draft-howe-vcon-wtf-extension-00.xml: draft-howe-vcon-wtf-extension.xml | versioned
-	sed -e 's/draft-howe-vcon-world-transcription-format.md-date/2025-09-25/g' -e 's/draft-howe-vcon-world-transcription-format.md-latest/draft-howe-vcon-world-transcription-format.md-00/g' -e 's/draft-howe-vcon-wtf-extension-date/2025-09-25/g' -e 's/draft-howe-vcon-wtf-extension-latest/draft-howe-vcon-wtf-extension-00/g' -e 's/draft-howe-vcon-wtf-extension-date/2025-09-25/g' -e 's/draft-howe-vcon-wtf-extension-latest/draft-howe-vcon-wtf-extension-00/g' $< >$@
+.INTERMEDIATE: versioned/draft-howe-vcon-wtf-extension-00.md
+versioned/draft-howe-vcon-wtf-extension-00.md: draft-howe-vcon-wtf-extension.md | versioned
+	sed -e 's/draft-howe-vcon-world-transcription-format.md-date/2025-09-25/g' -e 's/draft-howe-vcon-world-transcription-format.md-latest/draft-howe-vcon-world-transcription-format.md-00/g' -e 's/draft-howe-vcon-wtf-extension-date/2025-09-25/g' -e 's/draft-howe-vcon-wtf-extension-latest/draft-howe-vcon-wtf-extension-00/g' -e '/^{::include [^\/]/{ s/^{::include /{::include draft-howe-vcon-wtf-extension-00\//; }' $< >$@
+	for inc in $$(sed -ne '/^{::include [^\/]/{ s/^{::include draft-howe-vcon-wtf-extension-00\///;s/}$$//; p; }' $@); do \
+	  target=draft-howe-vcon-wtf-extension-00/$$inc; \
+	  mkdir -p $$(dirname "$$target"); \
+	  git show "$$tag:$$inc" >"$$target" || \
+	    (echo "Attempting to make a copy of $$inc"; \
+	     tmp=.; \
+	     make -C "$$tmp" "$$inc" && cp "$$tmp/$$inc" "$$target"; \
+	  ); \
+	done
